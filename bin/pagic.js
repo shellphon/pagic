d760555e7d15b5fcedd4270122a5604594bec6aa
shellphon <dont923@gmail.com>
2017-03-07 17:27:24 +0800

    watch file to rebuild

--- bin/pagic.js
+++ bin/pagic.js
@@ -4,7 +4,9 @@ const pkg = require('../package.json');
 const program = require('commander');
 const fs = require('fs');
 const Pagic = require('..').Pagic;
+const chokidar = require('chokidar');
 
+const Path = require('path');
 program
   .version(pkg.version)
   .option('-s, --src-dir [path]', 'Set src dir')
@@ -19,8 +21,91 @@ const pagic = new Pagic({
 
 pagic.build();
 
+const detectFile = function(path){
+   const ext = Path.extname(path);
+   if(ext!=='.js'){
+     return ext;
+   }
+   if(Path.basename(path,'.js')==='_layout'){
+      return 'layout';
+   }
+   return ext;
+};
+
 if (program.watch) {
-  fs.watch(pagic.options.srcDir, () => {
-    pagic.build();
+  var watcher = chokidar.watch(pagic.options.srcDir);
+
+  //init scan ready for watchfile
+  watcher.on('ready', () =>{
+    watcher.on('add', (path, stats) =>{
+      /*
+        新增md文件: 文件路径=>生成
+        ，其他文件 文件路径=>拷贝
+        ，layout文件 文件路径=> 从当前目录开始，所有md文件，如果md文件（除了当前目录）的目录没有layout文件，都重新生成
+      */
+      console.log('add:', path);
+      if(detectFile(path)==='.md'){
+        pagic.addMd(path);
+        return;
+      }
+
+      if(detectFile(path)==='layout'){
+        pagic.buildMDByAdd(path);
+        return;
+      }
+
+      pagic.copySingleFile(Path.relative(pagic.options.srcDir, path));
+       
+      
+      //pagic.build();
+      
+      
+    }).on('unlink', path =>{
+      /*
+        删除md文件: 文件路径=>删除html文件
+        删除其他文件: 文件路径=> 删除对应文件
+        删除layout文件: 文件路径=>当前目录开始，所有md文件，如果md文件目录没有layout，都重新生成
+      */
+      console.log('delete:',path);
+
+      if(detectFile(path)==='.md'){
+        pagic.delMd(Path.relative(pagic.options.srcDir, path));
+        return;
+      }
+
+      if(detectFile(path)==='layout'){
+        pagic.buildMDByDel(path);
+        return;
+      }
+
+      pagic.deleteStaticFile(Path.relative(pagic.options.srcDir, path));
+        
+      
+      //pagic.build();
+    }).on('change', (path, stats) =>{
+      /*
+        修改md文件：文件路径=>重新生成
+        修改其他文件：文件路径=>覆盖
+        修改layout文件：文件路径=> 当前目录开始，所有md文件，如果所在目录(当前目录除外)没有layout文件，则重新生成
+      */
+      console.log('change:', path);
+
+      if(detectFile(path)==='.md'){
+        pagic.addMd(path);
+        return;
+      }
+
+      if(detectFile(path)==='layout'){
+        pagic.buildMDByAdd(path, true);
+        return;
+      }
+
+      pagic.copySingleFile(Path.relative(pagic.options.srcDir, path));
+      
+      //pagic.build();
+    }).on('error', error =>{
+      console.log('watcher error: ${error}', error);
+      watcher.close();
+    });
   });
 }
--- src/index.js
+++ src/index.js
@@ -81,6 +81,191 @@ class Pagic {
     });
   }
 
+  addMd(filePath){
+    filePath = path.relative(this.options.srcDir, filePath);
+    this.buildMDFiles([filePath]);
+  }
+
+  buildMDByAdd(layoutPath, needFresh){
+    layoutPath = path.relative(this.options.srcDir, layoutPath);
+    const srcDir = path.resolve(this.options.srcDir, path.dirname(layoutPath));
+    const distDir = path.resolve(this.options.distDir, path.dirname(layoutPath));
+
+    let mdFiles = glob.sync('**/*.md', {
+      cwd: srcDir
+    });
+    if(needFresh){
+      this.refreshCache(path.resolve(srcDir, LAYOUT_FILENAME));
+    }
+
+    //filter sub has layout files
+    //console.log(srcDir);
+    mdFiles = mdFiles.filter(filePath => {
+      filePath = path.resolve(srcDir, filePath);
+      const fileDir = path.dirname(filePath);
+     // console.log(path.dirname(srcDir), fileDir);
+      //base md file should stay
+      if(srcDir===fileDir) return true;
+
+      const lydir = findParentDir.sync(filePath, LAYOUT_FILENAME);
+     // console.log(lydir, path.dirname(lydir));
+      if(lydir===fileDir+path.sep){
+        return false
+      }
+      return true;
+    });
+
+    if (mdFiles.length === 0) {
+      console.log('No markdown files found');
+      return;
+    }
+
+    mdFiles.forEach(filePath => {
+      const resolvedFilePath = path.resolve(srcDir, filePath);
+      const resolvedDistPath = path.resolve(distDir, filePath)
+        .replace(/\.md$/, '.html');
+
+      const layout = this.getLayout(resolvedFilePath);
+
+      if (!layout) {
+        console.error(`CANNOT find a layout for ${resolvedFilePath}, will skip this file`);
+        return;
+      }
+
+      const originalContent = fse.readFileSync(resolvedFilePath, 'utf-8');
+      
+      const startTime = new Date();
+
+      const context = processors.reduce((prevContext, processor) => processor(prevContext), {
+        path: filePath,
+        content: originalContent,
+        options: this.options,
+      });
+
+      const html = layout(context);
+
+      fse.outputFileSync(resolvedDistPath, html);
+
+      const endTime = new Date();
+
+      console.log(`Generated ${resolvedDistPath} ......`+(endTime.getTime()-startTime.getTime())+'ms');
+    });
+  }
+
+  delMd(filePath){
+    const distPath = path.resolve(this.options.distDir, filePath).replace(/\.md$/, '.html');
+    this.removeFile(distPath);
+  }
+
+  deleteStaticFile(filePath){
+    const distPath = path.resolve(this.options.distDir, filePath);
+    this.removeFile(distPath);
+  }
+
+  buildMDByDel(layoutPath){
+    layoutPath = path.relative(this.options.srcDir, layoutPath);
+    const srcDir = path.resolve(this.options.srcDir, path.dirname(layoutPath));
+    const distDir = path.resolve(this.options.distDir, path.dirname(layoutPath));
+
+    let mdFiles = glob.sync('**/*.md', {
+      cwd: srcDir
+    });
+
+    //filter sub has layout files
+    mdFiles = mdFiles.filter(filePath => {
+      filePath = path.resolve(srcDir, filePath);
+      const fileDir = path.dirname(filePath);
+     // console.log(fileDir);
+
+      const lydir = findParentDir.sync(filePath, LAYOUT_FILENAME);
+     // console.log(lydir, path.dirname(lydir));
+      if(lydir===fileDir+path.sep){
+        return false
+      }
+      return true;
+    });
+
+    if (mdFiles.length === 0) {
+      console.log('No markdown files found');
+      return;
+    }
+
+    mdFiles.forEach(filePath => {
+      const resolvedFilePath = path.resolve(srcDir, filePath);
+      const resolvedDistPath = path.resolve(distDir, filePath)
+        .replace(/\.md$/, '.html');
+
+      const layout = this.getLayout(resolvedFilePath);
+
+      if (!layout) {
+        console.error(`CANNOT find a layout for ${resolvedFilePath}, will skip this file`);
+        return;
+      }
+
+      const originalContent = fse.readFileSync(resolvedFilePath, 'utf-8');
+      
+      const startTime = new Date();
+
+      const context = processors.reduce((prevContext, processor) => processor(prevContext), {
+        path: filePath,
+        content: originalContent,
+        options: this.options,
+      });
+
+      const html = layout(context);
+
+      fse.outputFileSync(resolvedDistPath, html);
+
+      const endTime = new Date();
+
+      console.log(`Generated ${resolvedDistPath} ......`+(endTime.getTime()-startTime.getTime())+'ms');
+    });
+  }
+
+  //delete layoutjs should delete cache，modify should refresh cache
+  refreshCache(layoutPath, isRefresh){
+    delete require.cache[layoutPath];
+    isRefresh && require(layoutPath);
+  }
+
+  buildMDFiles(files){
+    if (files.length === 0) {
+      console.log('No markdown files found');
+      return;
+    }
+
+    files.forEach(filePath => {
+      const resolvedFilePath = path.resolve(this.options.srcDir, filePath);
+      const resolvedDistPath = path.resolve(this.options.distDir, filePath)
+        .replace(/\.md$/, '.html');
+
+      const layout = this.getLayout(resolvedFilePath);
+
+      if (!layout) {
+        console.error(`CANNOT find a layout for ${resolvedFilePath}, will skip this file`);
+        return;
+      }
+
+      const originalContent = fse.readFileSync(resolvedFilePath, 'utf-8');
+      
+      const startTime = new Date();
+
+      const context = processors.reduce((prevContext, processor) => processor(prevContext), {
+        path: filePath,
+        content: originalContent,
+        options: this.options,
+      });
+
+      const html = layout(context);
+
+      fse.outputFileSync(resolvedDistPath, html);
+
+      const endTime = new Date();
+
+      console.log(`Generated ${resolvedDistPath} ......`+(endTime.getTime()-startTime.getTime())+'ms');
+    });
+  }
+
   copyStaticFiles() {
     const staticFiles = glob.sync('**/*', {
       ignore: [
@@ -95,14 +280,21 @@ class Pagic {
       return;
     }
 
-    staticFiles.forEach(filePath => {
+    staticFiles.forEach(this.copySingleFile.bind(this));
+  }
+
+  copySingleFile(filePath){
       const resolvedFilePath = path.resolve(this.options.srcDir, filePath);
       const resolvedDistPath = path.resolve(this.options.distDir, filePath);
 
       fse.copySync(resolvedFilePath, resolvedDistPath);
 
       console.log(`Copied ${resolvedDistPath}`);
-    });
+  }
+
+  removeFile(filePath){
+    fse.removeSync(filePath);
+    console.log(`deleted ${filePath}`);
   }
 
   getLayout(currentPath) {
